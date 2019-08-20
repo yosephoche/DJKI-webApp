02d3be4cbac958afe61bbe048c5fb95fc54e8437
NoName03 <williamafif123@gmail.com>
2019-08-20 14:57:15 +0700

    penambahan fitur API Posts (EN dan ID)

--- app/Posts.php
+++ app/Posts.php
@@ -8,14 +8,13 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Posts extends Model
 {
     protected $table = 'posts';
-    protected $fillable = ['id_user', 'slug', 'title', 'content', 'keyword', 'image', 'category', 'status', 'comment' ];
+    protected $fillable = ['id_user', 'slug', 'title', 'content', 'keyword', 'image', 'category', 'status', 'comment', 'lang'];
 
     use SoftDeletes;
     protected $dates = ['deleted_at'];
 
     public function users()
     {
-       return $this->belongsTo('App\User','id');
+        return $this->belongsTo('App\User', 'id');
     }
-
 }
