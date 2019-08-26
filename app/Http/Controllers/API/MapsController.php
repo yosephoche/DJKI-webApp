fc779afe77cdce90591dbdeda7af110eac66614a
aryadiahmad4689 <sahrulafa45@gmail.com>
2019-08-26 09:27:23 +0800

    Maps Api

---
+++ app/Http/Controllers/API/MapsController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Maps;
+use General;
+use Response;
+
+class MapsController extends Controller
+{
+    // construk helper
+    public function __construct()
+    {
+        $this->helper = new General;
+        $this->response = new Response;
+    }
+    // get maps
+    public function getMaps(){
+        // get maps all
+        $maps = Maps::all();
+
+        // maps is not emtpty
+        if ($maps->isNotEmpty()) {
+            $response = $this->response->formatResponseWithPages("OK", $maps, $this->response->STAT_OK());
+            $headers = $this->response->HEADERS_REQUIRED('GET');
+            return response()->json($response, $this->response->STAT_OK());
+        } else {
+            $headers = $this->response->HEADERS_REQUIRED('GET');
+            $response = $this->response->formatResponseWithPages("Slide NOT FOUND", [], $this->response->STAT_NOT_FOUND());
+            return response()->json($response, $this->response->STAT_NOT_FOUND());
+        }
+
+
+    }
+}
