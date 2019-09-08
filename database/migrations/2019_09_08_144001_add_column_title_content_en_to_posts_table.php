c0e7bdcd016cf04e8475221cbacca10644d485d1
andri <andri@docotel.co.id>
2019-09-08 15:05:50 +0000

    update migrations

--- database/migrations/2019_09_08_144001_add_column_title_content_en_to_posts_table.php
+++ database/migrations/2019_09_08_144001_add_column_title_content_en_to_posts_table.php
@@ -14,9 +14,9 @@ class AddColumnTitleContentEnToPostsTable extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->text('title_en');
-            $table->longText('content_en');
-            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
+           $table->text('title_en');
+           $table->longText('content_en');
+            //$table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
@@ -27,10 +27,10 @@ class AddColumnTitleContentEnToPostsTable extends Migration
      */
     public function down()
     {
-        Schema::table('posts', function (Blueprint $table) {
+       Schema::table('posts', function (Blueprint $table) {
             $table->dropColumn('title_en');
             $table->dropColumn('content_en');
-            $table->dropForeign(['id_user']);
-        });
+            //$table->dropForeign(['id_user']);
+       });
     }
 }
