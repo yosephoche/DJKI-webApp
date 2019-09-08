9b3803c51b6356b6c4d83455488ddfb181ee45e5
ocdev <oche@docotel.co.id>
2019-09-08 22:47:20 +0800

    update migration

--- database/migrations/2019_09_08_144001_add_column_title_content_en_to_posts_table.php
+++ database/migrations/2019_09_08_144001_add_column_title_content_en_to_posts_table.php
@@ -13,7 +13,11 @@ class AddColumnTitleContentEnToPostsTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('posts', function (Blueprint $table) {
+            $table->text('title_en');
+            $table->longText('content_en');
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
+        });
     }
 
     /**
@@ -23,6 +27,10 @@ class AddColumnTitleContentEnToPostsTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('title_en');
+            $table->dropColumn('content_en');
+            $table->dropForeign(['id_user']);
+        });
     }
 }
